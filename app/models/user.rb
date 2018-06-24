d664dcc7b6eb0ac9cf67aa0b387dde2db6a69ec0
Andrew Dymond <andy.dymond@gmail.com>
2018-06-24 12:02:42 -0600

    Add default for role & enum to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates_presence_of :name, :email, :password
   validates_uniqueness_of :name, :email
   has_secure_password
+  enum role: [ :admin, :user ]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180624174907) do
+ActiveRecord::Schema.define(version: 20180624175157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180624174907) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.integer "role"
+    t.integer "role", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
