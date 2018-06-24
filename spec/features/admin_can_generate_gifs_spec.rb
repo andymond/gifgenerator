2443fee512133d914d8dad34049cd14368976411
Andrew Dymond <andy.dymond@gmail.com>
2018-06-24 13:44:15 -0600

    Add figaro for giphy_api_key

---
+++ spec/features/admin_can_generate_gifs_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+describe "admin can generate gifs" do
+  it "allows user with admin role to add random gifs db" do
+    user = create(:user, role: 0)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit user_path(user)
+    click_on "Generate new Gif!"
+
+    expect(current_path).to eq(gifs_path)
+    expect{Gif.all}.to eq(1)
+  end
+end
