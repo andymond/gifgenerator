323a9b3b93ac1cd9607d64c3d61e9a29afb73934
Andrew Dymond <andy.dymond@gmail.com>
2018-06-24 12:02:28 -0600

    Add test for enum & role default value

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe User, type: :model do
   it {should validate_presence_of(:email)}
   it {should validate_uniqueness_of(:email)}
   it {should validate_presence_of(:password)}
+  it {should define_enum_for(:role).with([:admin, :user])}
+  it "should have default role of user" do
+    user = User.create(name: "dog", email: "bacon@baconmail.com", password: "password")
+
+    expect(user.role).to eq("user")
+  end
 end
