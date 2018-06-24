3218e50e789c4704696ba5fc0e8a955a6995635e
Andrew Dymond <andy.dymond@gmail.com>
2018-06-24 14:00:36 -0600

    Update admin can generate gifs spec

--- spec/features/admin_can_generate_gifs_spec.rb
+++ spec/features/admin_can_generate_gifs_spec.rb
@@ -6,8 +6,11 @@ describe "admin can generate gifs" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit user_path(user)
+
+    fill_in "search_term", with: "cats"
     click_on "Generate new Gif!"
 
+    expect(page).to have_content("Generated new Gif!")
     expect(current_path).to eq(gifs_path)
     expect{Gif.all}.to eq(1)
   end
