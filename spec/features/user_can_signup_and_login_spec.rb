285e4279c6ffe2afe66a6a57db03454fb75d4fd4
Andrew Dymond <andy.dymond@gmail.com>
2018-06-24 12:33:31 -0600

    Update spec for user sign up

--- spec/features/user_can_signup_and_login_spec.rb
+++ spec/features/user_can_signup_and_login_spec.rb
@@ -4,13 +4,17 @@ describe "user can sign up" do
   it "allows unregistered user to create account" do
     visit root_path
 
-    fill_in ".name", with: "coffeeman"
-    fill_in ".email", with: "beans@beans.com"
-    fill_in ".password", with: "unbreakablepassword"
+    fill_in "user_name", with: "coffeeman"
+    fill_in "user_email", with: "beans@beans.com"
+    fill_in "user_password", with: "unbreakablepassword"
 
     click_on "Create Account"
 
-    expect(current_path).to eq(dashboard_path)
+    user = User.find_by(name: 'coffeeman')
+
+    expect(current_path).to eq(user_path(user))
     expect(page).to have_content("Account Successfully Created.")
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(user.email)
   end
 end
