fbcb0f4ebf0bb5eedf8c88c4c1589e266b99dee3
Andrew Dymond <andy.dymond@gmail.com>
2018-06-24 14:42:44 -0600

    Remove logic from gifs_controller to call giphy service, specs all pass

--- app/controllers/gifs_controller.rb
+++ app/controllers/gifs_controller.rb
@@ -2,10 +2,7 @@ class GifsController < ApplicationController
   before_action :admin?, only: [:create]
 
   def create
-    conn = Faraday.new
-    url = "https://api.giphy.com/v1/gifs/random?api_key=#{ENV["giphy_api_key"]}&tag=#{params[:search_term]}"
-    response = conn.get(url)
-    results = JSON.parse(response.body, symbolize_names: true)[:data]
+    results = GiphyService.new.random(params[:search_term])
     gif = Gif.new(slug: results[:slug], embed_url: results[:embed_url])
     if gif.save
       flash[:notice] = "Generated new Gif!"
