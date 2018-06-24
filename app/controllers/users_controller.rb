44f33ba3a29eb404cfd41da52dc1d5f468bd3b70
Andrew Dymond <andy.dymond@gmail.com>
2018-06-24 12:36:08 -0600

    Add sign up for users, spec passing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      flash[:notice] = "Account Successfully Created."
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Couldn't Create Account"
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
+
+end
