23d82e7eb980b944c51139da114d3f08b0288fe2
Andrew Dymond <andy.dymond@gmail.com>
2018-07-02 13:56:14 -0600

    Update users_can_logout_spec

--- spec/features/users_can_logout_spec.rb
+++ spec/features/users_can_logout_spec.rb
@@ -3,16 +3,19 @@ require "rails_helper"
 describe "All users can log out" do
   it "allows admin to logout" do
     admin = create(:user, role: 1)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
     visit root_path
+    click_on "Log in"
 
+    fill_in "email", with: admin.email
+    fill_in "password", with: admin.password
+
+    click_on("Log in")
     click_on("Log out")
 
-    expect(current_page).to have_content("Successfully logged out")
-    expect(current_page).to_not have_content("Log out")
+    expect(page).to have_content("Logged out! Smell ya later, User1")
+    expect(page).to_not have_content("Log out")
 
     visit user_path(admin)
-    expect(response.status).to eq(404)
+    expect(page.status_code).to eq(404)
   end
 end
