e4ffcce430b488f0add96c72831cb1aef542aeaf
Andrew Dymond <andy.dymond@gmail.com>
2018-07-02 13:56:54 -0600

    Add basic logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   resources :users, only: [:create, :show]
   resources :sessions, only: [:new, :create]
+  delete "/logout", to: 'sessions#destroy', as: 'logout'
   resources :gifs, only: [:create, :index]
 end
